4f11561aab8af4f475ed0f8c9350d637ef66ecd9
Heni0717 <906123@naver.com>
2025-01-14 19:45:02 +0900

    Comment: 주석 추가
    코드별 요구사항에 따른 주석 추가

--- src/main/java/com/example/Lv1/Main.java
+++ src/main/java/com/example/Lv1/Main.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 입/출력 반복
         while (true) {
             System.out.println("[ SHAKESHACK MENU ]");
             System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
@@ -17,10 +19,13 @@ public class Main {
             System.out.println("0. 종료      | 종료");
             System.out.print("원하는 메뉴 번호 입력: ");
             int input = sc.nextInt();
+
+            // 0번을 입력받으면 프로그램 종료
             if(input==0){
                 System.out.println("프로그램 종료");
                 break;
             }
+            // 입력된 번호에 따른 출력 로직(선택한 메뉴 : 메뉴이름) 출력
             switch (input){
                 case 1:
                     System.out.println("선택한 메뉴: ShackBurger");
