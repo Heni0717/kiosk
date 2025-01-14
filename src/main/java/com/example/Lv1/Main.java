095ce422757e7d4db60ecea8c6b9cdb54e638b13
Heni0717 <906123@naver.com>
2025-01-14 19:38:35 +0900

    Feat: Kiosk Lv1 구현
    요구사항 : (1) 햄버거 메뉴 출력 - 기본 출력구문
    	   (2) 제시된 메뉴중 입력 숫자에 따른 로직 실행 - switch
    	   	-> 선택한 메뉴:메뉴이름 으로 출력
    	   (3) 특정 번호가 입력되면 프로그램 종료 - while, if

--- src/main/java/com/example/Lv1/Main.java
+++ src/main/java/com/example/Lv1/Main.java
@@ -1,7 +1,43 @@
 package com.example.Lv1;
 
+import java.util.Scanner;
+
+// Lv1. 기본적인 키오스크 프로그래밍
+//  * 요구사항 : 햄버거 메뉴 출력, 입력받은 숫자에 따른 로직 실행, 특정 번호가 입력되면 프로그램 종료
+//          => 입력 처리, 간단한 흐름 제어
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("[ SHAKESHACK MENU ]");
+            System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+            System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+            System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+            System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+            System.out.println("0. 종료      | 종료");
+            System.out.print("원하는 메뉴 번호 입력: ");
+            int input = sc.nextInt();
+            if(input==0){
+                System.out.println("프로그램 종료");
+                break;
+            }
+            switch (input){
+                case 1:
+                    System.out.println("선택한 메뉴: ShackBurger");
+                    break;
+                case 2:
+                    System.out.println("선택한 메뉴: SmokeShack");
+                    break;
+                case 3:
+                    System.out.println("선택한 메뉴: Cheeseburger");
+                    break;
+                case 4:
+                    System.out.println("선택한 메뉴: Hamburger");
+                    break;
+                default:
+                    System.out.println("번호 입력 오류");
+                    continue;
+            }
+        }
     }
 }
