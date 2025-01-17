9bb205f9790a80f27e49522931b080b41c6717be
Heni0717 <906123@naver.com>
2025-01-17 13:59:35 +0900

    Docs: Kiosk Lv5 패키지, 클래스 추가

---
+++ src/main/java/com/example/Lv5/MenuItem.java
@@ -0,0 +1,16 @@
+package com.example.Lv5;
+
+// 햄버거 메뉴를 관리하는 MenuItem 클래스
+public class MenuItem {
+    // 필드 : 이름, 가격, 설명
+    String name;
+    double price;
+    String description;
+
+    // 인스턴스화 시킬 생성자
+    public MenuItem(String name, double price, String description){
+        this.name=name;
+        this.price=price;
+        this.description=description;
+    }
+}
